f44aacc3204c3c7a1db15394412063ce43e3f4cf
Daniil Sampir <sampir_danik@mail.ru>
2021-05-04 13:11:42 +0300

    Reworked pom to run tests from console, removed RandomNumbers

--- com.calculator.junit.first/src/test/java/com/calculator/junit/first/test/CalculatorEngineTest.java
+++ com.calculator.junit.first/src/test/java/com/calculator/junit/first/test/CalculatorEngineTest.java
@@ -3,7 +3,6 @@ package com.calculator.junit.first.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
